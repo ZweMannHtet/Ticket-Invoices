6e07e5659bdf5f31397257e6c8ee4c9f30c172c3
ZweMannHtet <zweman98240@gmail.com>
2023-10-10 02:15:55 +0630

    add navigation bar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <header
+    class="header flex flex-row z-50 bg-purple lg:min-h-full lg:min-w-24 lg:flex-col lg:rounded-r-3xl"
+  >
+    <div
+      class="branding flex rounded-r-2xl bg-pale-purple justify-center p-6 lg:w-full"
+    >
+      <img
+        class="logo w-auto h-8"
+        src="@/assets/file-invoice-dollar-solid.png"
+        alt=""
+      />
+    </div>
+  </header>
+</template>
+
+<script setup></script>
