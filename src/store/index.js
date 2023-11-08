ca6b638f71c569e29f2a04fa8b5099b91ce59388
ZweMannHtet <zweman98240@gmail.com>
2023-11-09 04:00:52 +0630

    flash model state update

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    invoiceModal: null
+    invoiceModal: null,
+    flashActive:false
   },
   getters: {
   },
   mutations: {
     TOGGLE_INVOICE(state){
       state.invoiceModal = !state.invoiceModal
+    },
+    TOGGLE_FLASH(state){
+      state.flashActive = !state.flashActive
     }
   },
   actions: {
